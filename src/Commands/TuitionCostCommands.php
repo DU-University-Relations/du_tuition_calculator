0a651de86e82c0b069c38e7f6cf7ce3ca47e39e5
tsheu2 <144848543+tsheu2@users.noreply.github.com>
2025-09-24 14:06:38 -0600

    Update TuitionCostCommands.php to use pantheon secret

--- src/Commands/TuitionCostCommands.php
+++ src/Commands/TuitionCostCommands.php
@@ -114,7 +114,10 @@ class TuitionCostCommands extends DrushCommands {
     // Config variables.
     $url = $config->get('api_url');
     $client_id = $config->get('client_id');
-    $client_secret = $config->get('client_secret');
+    $client_secret = \Drupal::service('key.repository')->getKey('tuition_calculator_key')->getKeyValue();
+    if (empty($client_secret)) {
+      $logger->error('Missing client secret from Key. Check key name/sync.');
+    }
 
     // Check for API URL before proceeding.
     if (empty($url)) {
